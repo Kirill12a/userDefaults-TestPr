e4eceea4578f8ef95116d6e8ef8337a6de95a106
Kirill12a <kirilldrozd12@gmail.com>
2021-11-20 21:38:12 +0300

    V 1.0 completed

--- userDefaults-ToDo-list/ViewController.swift
+++ userDefaults-ToDo-list/ViewController.swift
@@ -2,12 +2,66 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum KeyForDataBase: String {
+    case city
+    case street
+    case house
+    case build
+    case flat
+}
 
+class ViewController: UIViewController {
+    //MARK: Outlets
+    let defaults = UserDefaults.standard
+    
+    @IBOutlet weak var cityField: UITextField!
+    @IBOutlet weak var streetField: UITextField!
+    @IBOutlet weak var houseField: UITextField!
+    @IBOutlet weak var buildField: UITextField!
+    @IBOutlet weak var flatField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        cityField.text   = defaults.string(forKey: KeyForDataBase.city.rawValue)
+        streetField.text = defaults.string(forKey: KeyForDataBase.street.rawValue)
+        houseField.text  = defaults.string(forKey: KeyForDataBase.house.rawValue)
+        buildField.text  = defaults.string(forKey: KeyForDataBase.build.rawValue)
+        flatField.text   = defaults.string(forKey: KeyForDataBase.flat.rawValue)
+        //        sayHello(userAge: "23") { name in
+        //            print(name)
+        //        } comletion: {_ in
+        //            print("Hello all user in the worls")
+        //        }
     }
-
-
+    
+    //    func sayHello(userAge:String,sayHello: (String)-> Void, comletion: @escaping((String)->Void) ) {
+    //        let userName = "Kirill"
+    //        comletion(userName + userAge)
+    //    }
+    //
+    
+    
+    
+    @IBAction func saveAction(_ sender: Any) {
+        let city   =   cityField.text!
+        let street =   streetField.text!
+        let house  =   houseField.text!
+        let build  =   buildField.text!
+        let flat   =   flatField.text!
+        
+        if !city.isEmpty && !street.isEmpty && !house.isEmpty && !flat.isEmpty{
+            defaults.setValue(city,   forKey:   KeyForDataBase.city.rawValue)
+            defaults.setValue(street, forKey:   KeyForDataBase.street.rawValue)
+            defaults.setValue(house,  forKey:   KeyForDataBase.house.rawValue)
+            defaults.setValue(build,  forKey:   KeyForDataBase.build.rawValue)
+            defaults.setValue(flat,   forKey:   KeyForDataBase.flat.rawValue)
+            
+            
+        }
+        
+        
+    }
+    
 }
 
